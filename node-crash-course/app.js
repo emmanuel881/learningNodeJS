b35f715e032e9b672c17c9a99b6b9a68118641d7
Emmanuel Saura Ogola <emmanuelsaura007@gmail.com>
2024-05-08 14:44:40 +0300

    view eng

--- node-crash-course/app.js
+++ node-crash-course/app.js
@@ -1,12 +1,31 @@
+//write npm run loadSer to start the server file
+
 const express = require("express");
 const app = express();
 const port = 3000;
+
+//let us register a view engine
+app.set("view engine", "ejs");
 
 //returns an instance of a server
 app.listen(port, (req, res) => {
   console.log("running");
 });
 
-app.get("/", (req, res) => {
-  res.send("hello world");
+//sending a response act as a return thus the code doesn't progress
+app.get("/", (req, res, next) => {
+  //we get the name of our directory using __dirname
+  //res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  //res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about");
+});
+
+//404 error
+app.use((req, res) => {
+  //res.status(404).sendFile("./views/error404.html", { root: __dirname });
+  res.status(404).render("error404");
 });
