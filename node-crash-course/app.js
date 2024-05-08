b8531d27985842f881ef5b61bdfbe00b1a2bd0d2
Emmanuel Saura Ogola <emmanuelsaura007@gmail.com>
2024-05-08 19:35:07 +0300

    view eng

--- node-crash-course/app.js
+++ node-crash-course/app.js
@@ -24,6 +24,11 @@ app.get("/about", (req, res) => {
   res.render("about");
 });
 
+//create page
+app.get("/blogs/create", (req, res) => {
+  res.render("create");
+});
+
 //404 error
 app.use((req, res) => {
   //res.status(404).sendFile("./views/error404.html", { root: __dirname });
