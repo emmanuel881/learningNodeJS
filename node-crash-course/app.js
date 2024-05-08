a53ea9a3b37223cbadd6939275bd98d199408bfd
Emmanuel Saura Ogola <emmanuelsaura007@gmail.com>
2024-05-08 21:53:35 +0300

    middle ware

--- node-crash-course/app.js
+++ node-crash-course/app.js
@@ -3,13 +3,19 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const morgan = require("morgan");
+
+//middleware and static file(accessible by the browser)
+//the public word is the name of the folder we have allowed the broswer to access
+app.use(express.static("public", { root: __dirname }));
+app.use(morgan("dev"));
 
 //let us register a view engine
 app.set("view engine", "ejs");
 
 //returns an instance of a server
 app.listen(port, (req, res) => {
-  console.log("running");
+  console.log("running server instance");
 });
 
 //sending a response act as a return thus the code doesn't progress
