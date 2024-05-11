17658805d07da01361b14631be611760e3577317
Emmanuel Saura Ogola <emmanuelsaura007@gmail.com>
2024-05-11 13:45:36 +0300

    express

---
+++ express/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.listen(port, (req, res) => {
+  console.log("running server instance");
+});
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
