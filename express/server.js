6e107182e00c9db021fd01d4b2785f393d2ea1f7
Emmanuel Saura Ogola <emmanuelsaura007@gmail.com>
2024-05-11 14:26:13 +0300

    global variables

--- express/server.js
+++ express/server.js
@@ -1,11 +1,31 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
+const path = require("path");
+
+let posts = [
+  { id: 47, title: "Nairobi" },
+  { id: 44, title: "Migori" },
+  { id: 42, title: "Kisumu" },
+  { id: 40, title: "Busia" },
+  { id: 1, title: "Mombasa" },
+];
 
 app.listen(port, (req, res) => {
   console.log("running server instance");
 });
 
-app.get("/", (req, res) => {
-  res.send("hello world");
+app.get("/api/post", (re, res) => {
+  res.json(posts);
 });
+
+app.use(express.static(path.join(__dirname, "public")));
+/*
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "about.html"));
+});
+*/
